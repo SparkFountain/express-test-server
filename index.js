8cbdef47c35125e14b1c85d3b4da899adfbc4a0b
Spark Fountain <sparkfountain@yahoo.de>
2020-09-04 23:55:51 +0200

    added https support

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey = fs.readFileSync('./sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('./sslcert/server.crt', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// your express configuration here
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
+
+console.info('Server listening on 8080 (HTTP) and 8443 (HTTPS)');
